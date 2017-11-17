109f030e7082f75dda53476a13f15712e2a7c02a
martik32 <kieran.martin@roche.com>
2017-11-17 16:09:43 +0100

    add depreceation message

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage("This version is now deprecated! Please use the version found at https://github.roche.com/Rpackages/viewenhance for the latest version")
+}
