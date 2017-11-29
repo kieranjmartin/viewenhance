dfda9fdf9f88acb3c0ec69e1e5e97a7dad7d923e
Martin, Kieran {MDBA~Welwyn} <kieran.martin@roche.com>
2017-11-29 16:07:00 +0000

    Delete zzz.R

--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-    packageStartupMessage("This version is now deprecated! Please use the version found at https://github.roche.com/Rpackages/viewenhance for the latest version")
-}
